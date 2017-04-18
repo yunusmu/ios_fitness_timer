a0b77f78c4d5c998a14152e1038310fa7ef88181
Joseph Apps <joseph@Josephs-Mac.local>
2017-04-18 15:34:48 -0700

    Initial commit

--- FitnessTimer/ViewController.m
+++ FitnessTimer/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UIButton *speakButton;
 
 @end
 
@@ -25,5 +28,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)speakButtonTapped:(id)sender {
+    
+    AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc] init];
+    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:self.inputField.text];
+    [utterance setRate:0.5f];
+    [utterance setVoice:[AVSpeechSynthesisVoice voiceWithLanguage:@"tr-TR"]];
+    [synthesizer speakUtterance:utterance];
+}
 
 @end
