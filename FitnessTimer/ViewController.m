234153966460f0fb47f41a4dfb32b5792feb3aac
Joseph Apps <josephapps@Josephs-Mac.local>
2017-04-19 15:39:57 +0300

    Notifying user at given time intervals (specified via a UISlider) using an NSTimer.

--- FitnessTimer/ViewController.m
+++ FitnessTimer/ViewController.m
@@ -12,6 +12,14 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
 @property (weak, nonatomic) IBOutlet UIButton *speakButton;
+@property (weak, nonatomic) IBOutlet UILabel *timerIntervalValueLabel;
+@property (weak, nonatomic) IBOutlet UISlider *timerIntervalSlider;
+@property (weak, nonatomic) IBOutlet UILabel *counterLabel;
+
+@property (nonatomic) AVSpeechSynthesizer *speechSynthesizer;
+@property (nonatomic) NSTimer *counterTimer;
+@property (nonatomic) int counter;
+@property (nonatomic) int intervalCounter;
 
 @end
 
@@ -20,8 +28,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // initialize the speech synthesizer
+    self.speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+    self.timerIntervalValueLabel.text = [NSString stringWithFormat:@"%.0f", self.timerIntervalSlider.value];
+    self.counter = 0;
+    self.intervalCounter = 0;
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -35,6 +48,40 @@
     [utterance setRate:0.5f];
     [utterance setVoice:[AVSpeechSynthesisVoice voiceWithLanguage:@"tr-TR"]];
     [synthesizer speakUtterance:utterance];
+}
+
+- (IBAction)startButtonTapped:(id)sender {
+    
+    self.counterTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerTicked:) userInfo:nil repeats:YES];
+}
+
+- (void)timerTicked:(NSTimer*)timer {
+    
+    int timeInterval = [self.timerIntervalValueLabel.text intValue];
+    
+    ++self.counter;
+    
+    self.counterLabel.text = [NSString stringWithFormat:@"%d", self.counter];
+    
+    if(self.counter % timeInterval == 0) {
+        ++self.intervalCounter;
+        
+        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:[NSString stringWithFormat:@"%d", self.intervalCounter]];
+        
+        [utterance setRate:0.5f];
+        [utterance setVoice:[AVSpeechSynthesisVoice voiceWithLanguage:@"tr-TR"]];
+        
+        [self.speechSynthesizer speakUtterance:utterance];
+        
+        NSLog(@"Interval #%d", self.intervalCounter);
+    }
+}
+
+- (IBAction)timerIntervalSliderValueChanged:(id)sender {
+    
+    int refinedValue = (((int)self.timerIntervalSlider.value) / 5) * 5;
+    
+    self.timerIntervalValueLabel.text = [NSString stringWithFormat:@"%d", refinedValue];
 }
 
 @end
